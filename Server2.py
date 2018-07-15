aedfad1036ee379794801e4452ede6c074ea058e
yakir2a <yakir2a@gmail.com>
2018-07-16 00:24:10 +0300

    almost done with server
    added:
    create "user"
    adding new folders
    give file list
    search for file
    download from server
    goto to enter diffrent folders
    upload from client folder
    memory mange

--- Server2.py
+++ Server2.py
@@ -7,32 +7,50 @@ import time
 import pprint
 import json
 
+#hold root
 root = os.getcwd()
 
-def error_replay(clientsocket,Error):
-    clientsocket.sendall(b'0')
-    clientsocket.sendall(str(len(Error)).decode('utf8'))
-    clientsocket.sendall(Error.encode('utf8'))
+#flags
+FAILED = b'0'
+SUCCCESS = b'1'
+LOGGED = b'11'
 
-def send_data(clientsocket,data):
-    while count < 5:
-        count = 0
+#will update user config
+def update_user_memory(clientName,newspace):
+    global root
+    with open(root + '\\' + clientName + '.json','rw') as load:
+        userCon = json.load(load)
+        userCon['used_space'] = userCon['total_size'] - newspace
+        json.dump(userCon,load)
+
+#get error msg to send
+def error_replay(clientsocket,error):
+    #send FAILED flage to client
+    clientsocket.sendall(FAILED)
+
+    #send error data to client
+    send_data(clientsocket,error)
+
+#send data from server to client
+def send_data(clientsocket,data, size = None):
+
+    #send SUCC flage to client
+    clientsocket.sendall(SUCCCESS)
+
+    if size:
+        #calculate data size and incode it before send
         size = str(len(data)).encode('utf8')
-        clientsocket.sendall(size)
-        clientsocket.sendall(data.encode('utf8'))
-
-        if clientsocket.recv(size).decode('utf8') != size:
-            clientsocket.sendall(b'0')
-        else:
-            clientsocket.sendall(b'1')
-            break
     else:
-        error_replay(clientsocket,"Failed to send to long\n")
-
+        size = str(size.encode('utf8'))
+    #send data size
+    clientsocket.sendall(size)
+    #send data
+    clientsocket.sendall(data.encode('utf8'))
 
 
 #handle the clinets connection
 def client_thread(clientsocket, ip, port,serverID , MAX_BUFFER = 4096):       # MAX_BUFFER_SIZE is how big the message can be
+
 
     #recv user name from client
     size = clientsocket.recv(16)
@@ -43,7 +61,7 @@ def client_thread(clientsocket, ip, port,serverID , MAX_BUFFER = 4096):       # 
     if not os.path.exists(os.getcwd()+'/'+clientName):
         #if not create new folder for user
         os.mkdir(os.getcwd() + '/' + clientName)
-        clientsocket.sendall(b'1')
+        clientsocket.sendall(SUCCCESS)
         os.chdir('/'+clientName)
         size = clientsocket.recv(16)
         size = int(size, 2)
@@ -54,29 +72,37 @@ def client_thread(clientsocket, ip, port,serverID , MAX_BUFFER = 4096):       # 
         os.chdir('/' + clientName)
 
     #send 'connected signel'
-    clientsocket.sendall(b'11')
+    clientsocket.sendall(LOGGED)
+
+    global root
+    with open(root + '\\' + clientName + '.json','r') as load:
+        userCon = json.load(load)
+
+    current_free_space = userCon["total_size"] - userCon["used_space"]
 
     #waiting for commend to recvice
     option = clientsocket.recv(3)
 
+    #pick action base on option
     with Switch(option) as case:
         #create new folder
-        if case == b'000':
+        if case(b'0000'):
+            #incoming data size
             size = clientsocket.recv(16)
             size = int(size, 2)
-            size = clientsocket.recv(16)
-            size = int(size, 2)
-            clientName = clientsocket.recv(size).decode("utf8")
+            #incoming data folder path name
             folderPath = clientsocket.recv(size).decode("utf8")
             try:
-                os.mkdir(os.getcwd() + '/' +folderPath)
-                clientsocket.sendall(b'1')
+                os.mkdir(os.getcwd() + '\\' +folderPath)
+                clientsocket.sendall(SUCCCESS)
             except FileExistsError as e:
                 error_replay(clientsocket,"Folder already exsist by that path\n")
 
-        if case == b'001':
-            startpath = os.getcwd()
+        #get file list
+        if case(b'0001'):
+            startpath =  root + '\\' + clientName
             buffer = ''
+            #root - > current focuse folder, dirs - > folders in currnet folder, files - > file in current folder
             for root, dirs, files in os.walk(startpath):
                 level = root.replace(startpath, '').count(os.sep)
                 indent = ' ' * 4 * (level)
@@ -84,30 +110,99 @@ def client_thread(clientsocket, ip, port,serverID , MAX_BUFFER = 4096):       # 
                 subindent = ' ' * 4 * (level + 1)
                 for f in files:
                     buffer += '{}{}\n'.format(subindent, f)
+            send_data(clientsocket,buffer)
 
 
+        if case(b'0010'):
+            size = clientsocket.recv(16)
+            size = int(size, 2)
+            file = clientsocket.recv(size).decode("utf8")
+            buffer = ''
+            #go through all files in root dir in search of the file
+            for root, dirs, files in os.walk(startpath):
+                if file in files:
+                    buffer = root + '\\' + file
+            if buffer == '':
+                error_replay(clientsocket,"File not found\n")
+            else:
+                send_data(clientsocket,buffer)
+
+        #send file from server
+        if case(b'0011'):
+            #get recive data size
+            size = clientsocket.recv(16)
+            size = int(size, 2)
+            #get full path name
+            fileFullPath = clientsocket.recv(size).decode("utf8")
+
+            try:
+                file_to_send = open(fileFullPath,'rb')
+                file_size = os.path.getsize(fileFullPath)
+                send_data(clientsocket,file_to_send.read(),file_size)
+            except FileNotFoundError:
+                error_replay(clientsocket, "File not found\n")
+
+        #move into folder
+        if case(b'0100'):
+            size = clientsocket.recv(16)
+            size = int(size, 2)
+            #get folder to move into
+            goto = clientsocket.recv(size).decode("utf8")
+            try:
+                #try to move to chdir
+                os.chdir(goto)
+                clientsocket.sendall(SUCCCESS)
+            except OSError as e:
+                try:
+                    #failed so try from root dir
+                    os.chdir(root + '\\' + clientName + '\\' + goto)
+                    clientsocket.sendall(SUCCCESS)
+                except  OSError as e:
+                    #failed both local dir and root dir return error
+                    error_replay(clientsocket,"I/O error({0}): {1}".format(e.errno, e.strerror))
 
 
+        #load files from client folder to currnet folder
+        if case(b'0101'):
+            while True:
+                #recive file name size
+                size = clientsocket.recv(16)
+                size = int(size, 2)
+                #if no more data client will send 0 size
+                if size == 0:
+                    update_user_memory(clientName, current_free_space)
+                    break
+                #get file path to load
+                file = clientsocket.recv(size).decode("utf8")
+                file = os.getcwd() + '\\' + file
 
+                #recive file size
+                size = clientsocket.recv(16)
+                size = int(size, 2)
 
+                if (size/(1024*1024)) > current_free_space:
+                    error_replay(clientsocket,"Not enough free space")
+                    break
 
+                #create the dir if not already exists
+                os.makedirs(os.path.dirname(file), exist_ok=True)
+                buffer = size
+                with open(file, 'wb') as f:
+                    while buffer > 0:
+                        if buffer < MAX_BUFFER:
+                            recive_file = clientsocket.recv(buffer)
+                        else:
+                            recive_file = clientsocket.recv(MAX_BUFFER)
+                        f.write(recive_file)
+                        buffer -= MAX_BUFFER
+                print('File received successfully from Device')
 
-    size = int(size, 2)
-    filename = clientsocket.recv(size)              #gets the filename from client
-    filesize = clientsocket.recv(32)                #gets the file size from cleint
-    filesize = int(filesize, 2)
-    file_to_write = open(filename, 'wb')            #creating the received file on server side with the original name
+                # send SUCC flage to client
+                clientsocket.sendall(SUCCCESS)
+                current_free_space -= size/(1024*1024)
 
-    chunksize = 4096
-    while filesize > 0:                             #when filesize = 0, we received the entire file
-        if filesize < chunksize:                    #if server get large file
-            chunksize = filesize
-        data = clientsocket.recv(chunksize)
-        file_to_write.write(data)
-        filesize -= len(data)                        #subtrack what we received from the actual size
-
-    file_to_write.close()
-    print('File received successfully from Device')
+        #delete file
+        if case(b'0110'):
 
 
 
